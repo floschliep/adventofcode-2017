dead931f733245244db1aa41fe136bcdbee866c6
Florian Schliep <florian@appiculous.com>
2017-12-17 12:49:40 +0100

    Add Day 17 Part 2

--- AoC-17.playground/Contents.swift
+++ AoC-17.playground/Contents.swift
@@ -4,13 +4,30 @@ import Cocoa
 
 var position = 0
 let stepSize = 370
-var buffer = [0]
 
-for value in 1...2017 {
-    position = (position+stepSize)%buffer.count
+// - Part 1
+
+//var buffer = [0]
+//
+//for value in 1...2017 {
+//    position = (position+stepSize)%buffer.count
+//    position = position+1
+//    buffer.insert(value, at: position)
+//}
+//
+//let idx = buffer.index(of: 2017)!+1
+//assert(buffer[idx] == 1244)
+
+// - Part 2
+
+var valueAfter0 = 0
+
+for value in 0...50_000_000 {
+    position = (position+stepSize)%(value+1)
     position = position+1
-    buffer.insert(value, at: position)
+    if position == 1 {
+        valueAfter0 = value+1
+    }
 }
 
-let idx = buffer.index(of: 2017)!+1
-assert(buffer[idx] == 1244)
+assert(valueAfter0 == 11162912)
